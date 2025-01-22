5616d3ebd65f76792db1e703b943d1627b2e0df4
brandonhach <specialcambo@gmail.com>
2025-01-22 16:04:20 -0500

    call api getproducts and displayed to product component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../../types';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { ProductComponent } from '../../components/product/product.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  template: ` <p>home works!</p> `,
-  styleUrl: './home.component.scss',
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    ProductComponent,
+    MatGridListModule,
+    CommonModule,
+    MatIconModule,
+  ],
+  templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  products: Product[] = [];
   constructor(private productsService: ProductsService) {}
 
   /**
@@ -18,7 +31,7 @@ export class HomeComponent {
     this.productsService
       .getProducts('https://fakestoreapi.com/products', { limit: 5 })
       .subscribe((products: Product[]) => {
-        console.log(products);
+        this.products = products;
       });
   }
 }
