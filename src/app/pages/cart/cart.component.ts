9e4f13a285e40dfa3f80a601f8c2209271a1277b
brandonhach <specialcambo@gmail.com>
2025-01-27 21:31:23 -0500

    removed dummy data & connected data source for cart details to cartService

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Cart, CartItem } from '../../../types';
 import { CommonModule } from '@angular/common';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -8,44 +9,9 @@ import { CommonModule } from '@angular/common';
   templateUrl: './cart.component.html',
 })
 export class CartComponent {
-  cart: Cart = {
-    items: [
-      {
-        id: 1,
-        product: {
-          id: 1,
-          title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-          price: 109.95,
-          description:
-            'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-          category: "men's clothing",
-          image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-          rating: {
-            rate: 3.9,
-            count: 120,
-          },
-        },
-        quantity: 2,
-      },
-      {
-        id: 2,
-        product: {
-          id: 1,
-          title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
-          price: 109.95,
-          description:
-            'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-          category: "men's clothing",
-          image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-          rating: {
-            rate: 3.9,
-            count: 120,
-          },
-        },
-        quantity: 2,
-      },
-    ],
-  };
+  cart: Cart = { items: [] };
+
+  constructor(private cartService: CartService) {}
 
   getTotal(item: CartItem[]): number {
     return item
@@ -53,5 +19,9 @@ export class CartComponent {
       .reduce((prev, current) => prev + current, 0);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+    });
+  }
 }
