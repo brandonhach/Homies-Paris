ada68ab69f8bb69073701ddd587ddaf0f8cde801
brandonhach <specialcambo@gmail.com>
2025-01-29 12:43:23 -0500

    added new route: connoisseur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { ConnoisseurComponent } from './pages/connoisseur/connoisseur.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'connoisseur', component: ConnoisseurComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },
 ];
