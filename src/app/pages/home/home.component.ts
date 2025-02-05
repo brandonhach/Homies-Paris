3e64c02ccb999a97ef604db437058f7a991cc528
brandonhach <specialcambo@gmail.com>
2025-02-05 11:16:37 -0500

    refactor to simplify products into another component "shop"

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,39 +1,9 @@
 import { Component } from '@angular/core';
-import { ProductsService } from '../../services/products.service';
-import { Product } from '../../../types';
-import { ProductComponent } from '../../components/product/product.component';
-import { CommonModule } from '@angular/common';
-import { CartService } from '../../services/cart.service';
+import { ShopComponent } from '../shop/shop.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ProductComponent, CommonModule],
+  imports: [ShopComponent],
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
-  products: Product[] = [];
-
-  constructor(
-    private productsService: ProductsService,
-    private cartService: CartService
-  ) {}
-
-  /**
-   * Initialized when component is called
-   */
-  ngOnInit() {
-    this.productsService
-      .getProducts('https://fakestoreapi.com/products', { limit: 30 })
-      .subscribe((products: Product[]) => {
-        this.products = products;
-      });
-  }
-
-  onAddToCart(product: Product): void {
-    this.cartService.addToCart({
-      id: product.id,
-      product: product,
-      quantity: 1,
-    });
-  }
-}
+export class HomeComponent {}
