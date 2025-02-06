bdc68814160a7ddaf49d01690d44cbdd57145aef
brandonhach <specialcambo@gmail.com>
2025-02-06 12:47:35 -0500

    updated home page ui

---
+++ src/app/config/site-config.ts
@@ -0,0 +1,5 @@
+export const homeBannerItem = [
+  { imgUrl: '/home/1.jpg' },
+  { imgUrl: '/home/2.jpg' },
+  { imgUrl: '/home/3.jpg' },
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { ShopComponent } from '../shop/shop.component';
+import { CommonModule } from '@angular/common';
+import { homeBannerItem } from '../../config/site-config';
 
 @Component({
   selector: 'app-home',
-  imports: [ShopComponent],
+  imports: [ShopComponent, CommonModule],
   templateUrl: './home.component.html',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  homeBannerItem = homeBannerItem;
+}
