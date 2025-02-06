373c61f05c2e63530ee3203f1a3cdbdf6abf773a
brandonhach <specialcambo@gmail.com>
2025-02-06 15:30:53 -0500

    new dynamic route for product

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { ConnoisseurComponent } from './pages/connoisseur/connoisseur.component';
+import { ProductComponent } from './pages/product/product.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'cart', component: CartComponent },
   { path: 'connoisseur', component: ConnoisseurComponent },
+  { path: 'product/:productId', component: ProductComponent },
   { path: '', redirectTo: '', pathMatch: 'full' },
 ];
