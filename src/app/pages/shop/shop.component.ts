2e91f40a57ffea79a68187c19cfac1d7434eecf3
brandonhach <specialcambo@gmail.com>
2025-02-06 11:47:28 -0500

    var subscription for get products to reload ui when user sets a category

--- src/app/pages/shop/shop.component.ts
+++ src/app/pages/shop/shop.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../types';
 import { CartService } from '../../services/cart.service';
 import { StoreService } from '../../services/store.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shop',
@@ -14,29 +15,42 @@ export class ShopComponent {
   products: Product[] = [];
   sort = 'desc';
   count = '12';
+  category: string | undefined;
+  productsSubscription: Subscription | undefined;
 
   constructor(
     private cartService: CartService,
     private storeService: StoreService
   ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.getProducts();
   }
 
   private getProducts(): void {
-    this.storeService
-      .getAllProducts(this.count, this.sort)
+    this.productsSubscription = this.storeService
+      .getAllProducts(this.count, this.sort, this.category)
       .subscribe((products: Product[]) => {
         this.products = products;
       });
   }
 
-  public onAddToCart(product: Product): void {
+  onAddToCart(product: Product): void {
     this.cartService.addToCart({
       id: product.id,
       product: product,
       quantity: 1,
     });
+  }
+
+  onSetCategory(category: string): void {
+    this.category = category;
+    this.getProducts();
+  }
+
+  ngOnDestroy(): void {
+    if (this.productsSubscription) {
+      this.productsSubscription.unsubscribe();
+    }
   }
 }
