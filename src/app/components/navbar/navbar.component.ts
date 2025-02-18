74ffd4a1561e4087434ac35d8f94c160e61ec9f0
brandonhach <specialcambo@gmail.com>
2025-02-18 13:36:17 -0500

    ♻️ Refactored the HTML to prepare for cart indicator implementation

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { lucideShoppingCart, lucideUser, lucideGithub } from '@ng-icons/lucide';
+import { CartService } from '../../services/cart.service';
+import { CommonModule } from '@angular/common';
+import { Cart } from '../../../types';
 
 @Component({
   selector: 'app-navbar',
-  imports: [NgIcon],
+  imports: [NgIcon, CommonModule],
   templateUrl: './navbar.component.html',
   viewProviders: [
     provideIcons({ lucideShoppingCart, lucideUser, lucideGithub }),
