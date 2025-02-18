c07f1d998eaaff39de17f8fb03257d232530f477
brandonhach <specialcambo@gmail.com>
2025-02-18 13:38:51 -0500

    🎉 ExpressJS server for stripe payments

---
+++ server/app.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const cors = require("cors");
+const bodyparser = require("body-parser");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors({ origin: true, credentials: true }));
+
+const stripe = require("stripe")(
+  "sk_test_51QttefPD9ZFhA2S59NMsUqs9ntdGt6YfrrDHZ1F9PCnLuLErQP67W9qog0UKINI3X0VQ53Er1OQQo5WEnqHQbvd400RlTDYVu5"
+);
+
+app.post("/checkout", async (req, res, next) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      shipping_address_collection: {
+        allowed_countries: ["US", "CA"],
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 0,
+              currency: "usd",
+            },
+            display_name: "Free shipping",
+            // Delivers between 5-7 business days
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 5,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 7,
+              },
+            },
+          },
+        },
+        {
+          shipping_rate_data: {
+            type: "fixed_amount",
+            fixed_amount: {
+              amount: 1500,
+              currency: "usd",
+            },
+            display_name: "Next day air",
+            // Delivers in exactly 1 business day
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 1,
+              },
+              maximum: {
+                unit: "business_day",
+                value: 1,
+              },
+            },
+          },
+        },
+      ],
+      line_items: req.body.items.map((item) => ({
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.product.title,
+            images: [item.product.image],
+          },
+          unit_amount: item.product.price * 100,
+        },
+        quantity: item.quantity,
+      })),
+      mode: "payment",
+      success_url: "http://localhost:4242/success.html",
+      cancel_url: "http://localhost:4242/cancel.html",
+    });
+
+    res.status(200).json(session);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.listen(4242, () => console.log("app is running"));
